cd0709ba8daabacba1bd16217ed6e07919e52fd9
LapuwkaKuba <lapuwkakuba@gmail.com>
2024-11-13 15:30:28 +0100

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🌐 OGLĄDA | ☁ .gg/cloudshopek"];
+const statusMessages = ["OGLĄDA | 🌐 .gg/crazyshop"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
